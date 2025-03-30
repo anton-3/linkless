13c8e17c067b878b66e9b0865744712a82112fea
Anton Angeletti <angelettianton@gmail.com>
2025-03-30 02:53:18 -0500

    styling is now acceptable

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { generateLink, GenerateLinkActionState } from "./actions";
 
 const initialState: GenerateLinkActionState = {
   message: "",
-  link: "https://lnk.dedyn.io/ASDF",
+  link: "",
 };
 
 export default function HomePage() {
@@ -17,28 +17,37 @@ export default function HomePage() {
   };
 
   return (
-    <div className="w-[70%] py-32 m-auto text-[32px]">
-      <form
-        action={formAction}
-        className="flex flex-col items-center space-y-4"
-      >
-        <div className="flex flex-row">
-          <label htmlFor="link">link &gt;</label>
+    <main className="w-[70%] py-32 m-auto text-2xl sm:text-4xl">
+      <div className="flex flex-col items-center space-y-6">
+        <form
+          action={formAction}
+          className="w-full flex flex-row justify-center items-center"
+        >
+          <label htmlFor="link" className="basis-28 whitespace-nowrap">
+            link &gt;
+          </label>
           <input
             type="url"
             id="link"
             name="link"
             autoFocus
             required
-            className="w-2xl ml-4 px-1 py-0.5 border-b-white border-2 outline-none"
+            className="w-[70%] h-14 ml-4 px-1 py-0.5 border-b-white border-1 outline-none"
           />
-        </div>
+        </form>
         <p className="text-red-600">{state?.message}</p>
-        <div className="flex flex-row">
-          <button onClick={handleCopy}>copy &gt;</button>
-          <p className=" ml-4 break-all">{state?.link}</p>
-        </div>
-      </form>
-    </div>
+        {state.link && (
+          <div className="w-full flex flex-row justify-center items-center">
+            <button
+              onClick={handleCopy}
+              className="h-14 basis-28 border-white border-1 px-2.5 cursor-pointer"
+            >
+              copy
+            </button>
+            <p className="ml-6 break-all">{state?.link}</p>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
