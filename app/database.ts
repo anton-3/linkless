15c75aa7d80038e18c4525211cec0d605d11b812
Anton Angeletti <angelettianton@gmail.com>
2025-04-09 01:56:44 -0500

    caching

--- app/database.ts
+++ app/database.ts
@@ -1,15 +1,26 @@
 "use server";
 import getFirebaseDatabase from "./firebaseAdmin";
 
+const cache = new Map<string, string>();
 const database = await getFirebaseDatabase();
 const linksRef = database.ref(
   process.env.NODE_ENV === "production" ? "links" : "devlinks"
 );
 
 async function getValue(key: string): Promise<string | null> {
+  if (cache.has(key)) {
+    return cache.get(key)!;
+  }
+
   try {
     const snapshot = await linksRef.child(key).get();
-    return snapshot.exists() ? String(snapshot.val()) : null;
+    if (snapshot.exists()) {
+      const value = String(snapshot.val());
+      cache.set(key, value);
+      return value;
+    } else {
+      return null;
+    }
   } catch (error) {
     console.error(`firebase error fetching key ${key}`, error);
     return null;
@@ -19,6 +30,7 @@ async function getValue(key: string): Promise<string | null> {
 async function setValue(key: string, value: string): Promise<boolean> {
   try {
     await linksRef.child(key).set(value);
+    cache.set(key, value);
     return true;
   } catch (error) {
     console.error(`firebase error setting key ${key} = ${value}`, error);
